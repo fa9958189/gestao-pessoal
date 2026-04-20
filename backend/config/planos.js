d2237db8bb73439fae0cbcbe4b30ad026ed07664
felipe <130618161+fa9958189@users.noreply.github.com>
2026-04-20 20:22:53 -0300

    Atualiza valores dos planos: NORMAL=80, PROMO=49.90 e padroniza sistema

---
+++ backend/config/planos.js
@@ -0,0 +1,12 @@
+const PLANOS = Object.freeze({
+  NORMAL: {
+    nome: "Plano Normal",
+    valor: 80.0,
+  },
+  PROMO: {
+    nome: "Plano Promocional",
+    valor: 49.9,
+  },
+});
+
+export default PLANOS;
